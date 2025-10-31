d55df16941d80e744b28a2504967b43f6b8d2fd7
Yesenia Ramirez <ramirezy0415@gmail.com>
2025-10-30 20:42:31 -0500

    feature: GET /tracks/:id

--- api/tracks.js
+++ api/tracks.js
@@ -2,12 +2,23 @@ import express from "express";
 const tracksRouter = express.Router();
 export default tracksRouter;
 
-import { getTracks } from "#db/queries/tracks";
+import { getTracks, getTracksById } from "#db/queries/tracks";
 
 tracksRouter.get("/", async (req, res) => {
   try {
     console.log("Retrieving all tracks");
     const tracks = await getTracks();
+    res.status(200).json(tracks);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+tracksRouter.get("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    console.log("Retrieving track with id: ", id);
+    const tracks = await getTracksById(id);
     res.status(200).json(tracks);
   } catch (error) {
     console.error(error);
--- db/queries/tracks.js
+++ db/queries/tracks.js
@@ -10,3 +10,17 @@ export async function getTracks() {
     throw Error(error);
   }
 }
+
+export async function getTracksById(id) {
+  try {
+    const query = `SELECT * FROM tracks WHERE id = $1`;
+    const values = [id];
+    const {
+      rows: [track],
+    } = await db.query(query, values);
+    return track;
+  } catch (error) {
+    console.error(error);
+    throw Error(error);
+  }
+}
